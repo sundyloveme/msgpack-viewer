db0a529e8f6e4d3b58644e4e0835fdab4b90e09f
chenlongli <656233622@qq.com>
2023-03-29 16:00:29 +0800

    完成基本功能

--- src/main/kotlin/com/github/sundyloveme/msgpackviewer/services/MsgpackData.kt
+++ src/main/kotlin/com/github/sundyloveme/msgpackviewer/services/MsgpackData.kt
@@ -3,8 +3,11 @@ package com.github.sundyloveme.msgpackviewer.services
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
 import com.intellij.openapi.ui.Messages
+import org.msgpack.core.MessagePack
 import java.awt.Toolkit
 import java.awt.datatransfer.DataFlavor
+import java.io.ByteArrayInputStream
+
 
 class MsgpackData : AnAction() {
     override fun actionPerformed(e: AnActionEvent) {
@@ -12,6 +15,26 @@ class MsgpackData : AnAction() {
         val clipboardData = clipboard.getData(DataFlavor.stringFlavor) as String
         val firstLine = clipboardData.lines().firstOrNull() ?: ""
 
-        Messages.showMessageDialog("First line of clipboard data: $firstLine", "Clipboard Data", Messages.getInformationIcon())
+        val byteArray = hexStringToByteArray(firstLine.substring(2))
+        val unpacker = MessagePack.newDefaultUnpacker(ByteArrayInputStream(byteArray))
+        try {
+            val value = unpacker.unpackValue()
+            Messages.showMessageDialog("First line of clipboard data: $value", "Clipboard Data", Messages.getInformationIcon())
+        } catch (e: Exception) {
+            Messages.showMessageDialog("unpack failed. error is : $e", "Clipboard Data", Messages.getInformationIcon())
+        } finally {
+            unpacker.close()
+        }
+    }
+
+    private fun hexStringToByteArray(s: String): ByteArray {
+        val len = s.length
+        val data = ByteArray(len / 2)
+        var i = 0
+        while (i < len) {
+            data[i / 2] = ((Character.digit(s[i], 16) shl 4) + Character.digit(s[i + 1], 16)).toByte()
+            i += 2
+        }
+        return data
     }
 }
